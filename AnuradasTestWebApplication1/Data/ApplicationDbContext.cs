8497518f52bc7dbfb6797fae8dad48c28a3f2ab1
ACER <anuradasenaratne23@gmail.com>
2025-07-31 10:26:48 +0530

    Add project files.

---
+++ AnuradasTestWebApplication1/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using AnuradasTestWebApplication1.Models;
+
+namespace AnuradasTestWebApplication1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<AnuradasTestWebApplication1.Models.Jokes> Jokes { get; set; } = default!;
+    }
+}
---
+++ AnuradasTestWebApplication1/Models/Jokes.cs
@@ -0,0 +1,16 @@
+﻿namespace AnuradasTestWebApplication1.Models
+{
+    public class Jokes
+    {
+        public int Id { get; set; }
+
+        public String JokeQuestion { get; set; }
+
+        public String  JokeAnswer { get; set; }
+        
+        public Jokes()
+        {
+            
+        }
+    }
+}
